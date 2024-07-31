fdd0acc3e1a5cd02df1bad00fc1facdd28cf8ea6
MuhammadOwais <141480957+Muhammad-Owais1@users.noreply.github.com>
2024-07-31 12:50:46 +0500

    Animation Added.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,9 +4,11 @@ function App() {
   return (
     <>
       <div className="h-screen flex items-center justify-center flex-col gap-10">
-        <img className="w-[250px]" src={Logo} alt="" />
-        <h1 className="font-bold text-[40px] text-center font-sans">
-          WEBSITE COMMING SOON
+        <div className="bg-[#ffffff4e] w-full flex items-center justify-center py-4 ">
+          <img className="w-[250px]" src={Logo} alt="" />
+        </div>
+        <h1 className="font-bold text-[40px] text-center font-sans text-white">
+          WEBSITE COMING SOON
         </h1>
       </div>
     </>
