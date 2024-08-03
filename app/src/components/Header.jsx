c6d41c46d75c13f287167e681e5511e1fe073069
MuhammadOwais <141480957+Muhammad-Owais1@users.noreply.github.com>
2024-08-03 17:36:20 +0500

    Hero section completed.

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -3,14 +3,13 @@ import BannerLogo from "../imgs/BannerLogo.png";
 import Logo from "../imgs/Logo.png";
 
 export default function Header() {
-
-  const [navOpen, setNavOpen] = useState(false)
+  const [navOpen, setNavOpen] = useState(false);
 
   const [navFixed, setNavFixed] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY >= 74) {
+      if (window.scrollY >= 86) {
         setNavFixed(true);
       } else {
         setNavFixed(false);
@@ -26,7 +25,8 @@ export default function Header() {
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth >= 640) { // Assuming 640px as the sm breakpoint
+      if (window.innerWidth >= 640) {
+        // Assuming 640px as the sm breakpoint
         setNavOpen(false);
       }
     };
@@ -36,13 +36,13 @@ export default function Header() {
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, [])
+  }, []);
 
   return (
-   <>
+    <>
       <div className=" bg-slate-950 h-[86px] p-0 hidden sm:flex items-center justify-around flex-row gap-8  relative">
         <img className="w-[200px]" src={BannerLogo} alt="" />
-        <div className="flex fle-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
           <h1 className="font-black text-white sm:text-[40px] text-lg font-['Roboto'] italic ">
             WE ARE HERE.
           </h1>
@@ -58,42 +58,57 @@ export default function Header() {
         </div>
       </div>
 
-
-
-      <nav className={` w-full flex items-center justify-around flex-col sm:flex-row  sm:order-2  border-b-[1px] border-gray-200 {${navOpen ? "gap-0" : "gap-5"}} bg-white  top-0 z-10 sm:z-0 ${navFixed ? "fixed top-0 left-0 right-0" : ""}`}>
-        <div className="flex items-center justify-between w-full sm:w-auto px-4 py-5">
-          <img className="w-36" src={Logo} alt="" />
-          <button onClick={() => setNavOpen(navOpen ? false : true)} className="flex sm:hidden">x</button>
-        </div>
-        <div className={`flex justify-center sm:visible sm:h-auto sm:gap-20 gap-3 flex-col sm:flex-row sm:py-0  ${navOpen ? "h-[271px] visible" : "h-0 invisible"}  overflow-hidden `}>
-          <ul className="flex items-center justify-center gap-4 flex-col sm:flex-row">
-            {["home", "about us", "portfolio", "services", "join us"].map(
-              (item, index) => (
-                <li key={index}>
-                  <a
-                    href=""
-                    className="uppercase text-xs font-semibold font-['Poppins'] hover:text-cyan-600"
-                  >
-                    {item}
-                  </a>
-                  <div className=" bg-cyan-600"></div>
-                </li>
-              )
-            )}
-          </ul>
-          <button className="nav-contact-btn border-cyan-600 border-2 py-2 px-3 rounded-full relative overflow-hidden ">
-            <div className="bg-cyan-600 rounded-full absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-0"></div>
-            <a
-              href=""
-              className="uppercase text-xs font-semibold font-['Poppins'] h-full w-full flex items-center justify-center relative z-10"
+      <div className="h-[73px]">
+        <nav
+          className={` w-full flex items-center justify-around flex-col sm:flex-row bg-white  border-b-[1px] border-gray-200 {${
+            navOpen ? "gap-0" : "gap-5"
+          }} bg-white  top-0 z-10  ${
+            navFixed ? "fixed top-0 left-0 right-0" : ""
+          }`}
+        >
+          <div className="flex items-center justify-between w-full sm:w-auto px-4 py-5">
+            <img className="w-36" src={Logo} alt="" />
+            <button
+              onClick={() => setNavOpen(navOpen ? false : true)}
+              className="flex sm:hidden"
             >
-              contact us
-            </a>
-          </button>
-        </div>
-      </nav>
+              x
+            </button>
+          </div>
+          <div
+            className={`flex justify-center sm:visible sm:h-auto sm:gap-20 gap-3 flex-col sm:flex-row sm:py-0  ${
+              navOpen ? "h-[271px] visible" : "h-0 invisible"
+            }  overflow-hidden `}
+          >
+            <ul className="flex items-center justify-center gap-4 flex-col sm:flex-row">
+              {["home", "about us", "portfolio", "services", "join us"].map(
+                (item, index) => (
+                  <li key={index}>
+                    <a
+                      href={`#${item}`}
+                      className="uppercase text-xs font-semibold font-['Poppins'] hover:text-cyan-600"
+                    >
+                      {item}
+                    </a>
+                    <div className=" bg-cyan-600"></div>
+                  </li>
+                )
+              )}
+            </ul>
+            <button className="nav-contact-btn border-cyan-600 border-2 py-2 px-3 rounded-full relative overflow-hidden ">
+              <div className="bg-cyan-600 rounded-full absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-0"></div>
+              <a
+                href=""
+                className="uppercase text-xs font-semibold font-['Poppins'] h-full w-full flex items-center justify-center relative z-10"
+              >
+                contact us
+              </a>
+            </button>
+          </div>
+        </nav>
+      </div>
 
-      <div className=" bg-slate-950  h-auto  p-4 sm:hidden flex items-center justify-around flex-col  gap-8  ">
+      <div className=" bg-slate-950  h-auto  p-4 sm:hidden flex items-center justify-around flex-col  gap-8  relative -z-10">
         <img className="w-[200px]" src={BannerLogo} alt="" />
         <div className="flex fle-col items-center justify-center">
           <h1 className="font-black text-white sm:text-[40px] text-lg font-['Roboto'] italic ">
@@ -110,6 +125,6 @@ export default function Header() {
           </h2>
         </div>
       </div>
-      </>
+    </>
   );
 }
---
+++ app/src/components/HeroSection.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+import UI1 from "../imgs/ui1.png";
+import UI2 from "../imgs/ui2.jpg";
+import UI3 from "../imgs/ui3.png";
+import UI4 from "../imgs/ui4.jpg";
+import UI5 from "../imgs/ui5.png";
+import UI6 from "../imgs/ui6.png";
+import UI7 from "../imgs/ui7.png";
+
+export default function HeroSection() {
+  return (
+    <div id="home" className="pt-14 px-20 w-screen relative">
+      <h1 className="font-['Poppins'] mb-10 font-bold text-[25px] sm:text-[40px] text-center px-0 sm:px-20">
+        Come On. Don't Think Too Much. Get Your Solution. Get Your First Job.
+      </h1>
+
+      <div className="h-full  p-1 hero-tile-y hidden flex-col items-center justify-center ">
+        <div className="transition-[0.3s] hover:h-[242px] hover:w-[252px] h-[218px] w-[212px] bg-cyan-400 rounded-3xl flex items-center justify-center">
+          <img src={UI2} className="w-[80%]" alt="" />
+        </div>
+      </div>
+
+      <div className="h-full sm-hero-tile-y p-1 hidden flex-col items-center justify-center gap-3">
+        <div className="h-[211px] w-[212px] hover:h-[244px] hover:w-[252px] transition-[0.3s] bg-cyan-500 rounded-3xl flex items-center justify-center">
+          <img className="w-[80%]" src={UI3} alt="" />
+        </div>
+        <div className="h-[128px] w-[212px] transition-[0.3s] hover:h-[200px] hover:w-[252px] box-content bg-cyan-300 rounded-3xl flex items-center justify-center">
+          <img src={UI1} className="w-[80%]" alt="" />
+        </div>
+      </div>
+
+      <div className=" flex items-center justify-center w-full  min-h-[450px] gap-[10px]">
+        <div className="hero-tile-x h-full  p-1 flex flex-col items-center justify-center ">
+          <div className="transition-[0.3s] hover:h-[242px] hover:w-[252px] h-[218px] w-[212px] bg-cyan-400 rounded-3xl flex items-center justify-center">
+            <img src={UI2} className="w-[80%]" alt="" />
+          </div>
+        </div>
+
+        <div className="h-full sm-hero-tile-x p-1 flex flex-col items-center justify-center gap-3">
+          <div className="h-[211px] w-[212px] hover:h-[244px] hover:w-[252px] transition-[0.3s] bg-cyan-500 rounded-3xl flex items-center justify-center">
+            <img className="w-[80%]" src={UI3} alt="" />
+          </div>
+          <div className="h-[128px] w-[212px] transition-[0.3s] hover:h-[200px] hover:w-[252px] box-content bg-cyan-300 rounded-3xl flex items-center justify-center">
+            <img src={UI1} className="w-[80%]" alt="" />
+          </div>
+        </div>
+        {/* hover:px-5 hover:py-14 */}
+        <div className=" h-full  p-1 flex flex-col items-center justify-center">
+          <div className=" hover:w-[252px] hover:h-[490px] hover:py-7 transition-[0.3s]   h-[442px] w-[212px]  bg-cyan-700 rounded-3xl flex items-center justify-center">
+            <img src={UI7} className="w-[80%]" alt="" />
+          </div>
+        </div>
+
+        <div className="h-full sm-hero-tile-x  p-1 flex flex-col items-center justify-center gap-3">
+          <div className="h-[128px] w-[212px] transition-[0.3s] hover:h-[219px] hover:w-[252px] bg-cyan-600 rounded-3xl flex items-center justify-center">
+            <img src={UI5} className="w-[70%]" alt="" />
+          </div>
+          <div className="transition-[0.3s] hover:h-[322px] hover:w-[252px] h-[211px] w-[212px] bg-cyan-800 rounded-3xl flex items-center justify-center">
+            <img className="h-[80%]" src={UI6} alt="" />
+          </div>
+        </div>
+
+        <div className="hero-tile-x h-full p-1 flex flex-col items-center justify-center ">
+          <div className="transition-[0.3s] hover:h-[330px] hover:w-[252px] h-[218px] w-[212px] bg-cyan-200 rounded-3xl flex items-center justify-center">
+            <img src={UI4} className="w-[70%]" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="h-full sm-hero-tile-y  p-1 hidden flex-col items-center justify-center gap-3">
+        <div className="h-[128px] w-[212px] transition-[0.3s] hover:h-[219px] hover:w-[252px] bg-cyan-600 rounded-3xl flex items-center justify-center">
+          <img src={UI5} className="w-[70%]" alt="" />
+        </div>
+        <div className="transition-[0.3s] hover:h-[322px] hover:w-[252px] h-[211px] w-[212px] bg-cyan-800 rounded-3xl flex items-center justify-center">
+          <img className="h-[80%]" src={UI6} alt="" />
+        </div>
+      </div>
+
+      <div className="hero-tile-y h-full p-1 hidden flex-col items-center justify-center ">
+        <div className="transition-[0.3s] hover:h-[330px] hover:w-[252px] h-[218px] w-[212px] bg-cyan-200 rounded-3xl flex items-center justify-center">
+          <img src={UI4} className="w-[70%]" alt="" />
+        </div>
+      </div>
+      <div className="absolute left-0 top-[150px] -z-20">
+        <Marquee>
+          {[
+            "WE DEVELOP.",
+            "WE DEVELOP.",
+            "WE DEVELOP.",
+            "WE DEVELOP.",
+            "WE DEVELOP.",
+          ].map((item, index) => (
+            <p
+              key={index}
+              className="ml-[150px] text-[80px] text-gray-100 font-black font-['Poppins']"
+            >
+              {item}
+            </p>
+          ))}
+        </Marquee>
+      </div>
+      <Marquee>
+        {[
+          "GET YOUR WEBSITE READY NOW.",
+          "GET YOUR APP READY NOW.",
+          "JOIN US AND EARN.",
+          "GET CODING TASKS.",
+          "DO OUR MARKETING.",
+        ].map((item, index) => (
+          <p className="font-black font-['Poppins'] ml-5" key={index}>
+            {item}
+          </p>
+        ))}
+      </Marquee>
+    </div>
+  );
+}
